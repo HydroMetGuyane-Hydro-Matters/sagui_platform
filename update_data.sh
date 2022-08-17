ebd85edbd4c3da8275eac97e1987b93ea636e75e
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2022-08-17 17:01:47 +0200

    [fix] Remove colors in bash output (mixes badly with crontab tasks)

--- update_data.sh
+++ update_data.sh
@@ -6,8 +6,10 @@ export GROUP_ID="$(id -g)"
 # cd into the current script path
 # useful for cron, which does not know HOME
 cd "$(dirname "$0")";
-ORANGE=$(tput setaf 3)
-NORMAL=$(tput sgr0) # taken from https://stackoverflow.com/questions/4332478/read-the-current-text-color-in-a-xterm/4332530#4332530
+#ORANGE=$(tput setaf 3)
+#NORMAL=$(tput sgr0) # taken from https://stackoverflow.com/questions/4332478/read-the-current-text-color-in-a-xterm/4332530#4332530
+ORANGE=
+NORMAL=
 
 echo $(date +'%d-%m-%Y_%H:%M')
 
@@ -19,15 +21,15 @@ printf "Run scheduler\n${NORMAL}"
 # start doesn't work, since restart command returns immediately (doesn't wait for the end of the run process)
 # so we change the pattern: scheduler runs as 'ghost' container that we cn exec when needed.
 # exec'ing happens in the terminal
-/usr/local/bin/docker-compose -f docker-compose.yml -f docker-compose.prod.yml exec scheduler bash run.sh
+/usr/local/bin/docker-compose -f docker-compose.yml -f docker-compose.prod.yml exec -T scheduler bash run.sh
 
 printf "${ORANGE}\n\n-------------------------------------------------\n"
 printf "Import scheduler data into DB\n${NORMAL}"
-/usr/local/bin/docker-compose -f docker-compose.yml  -f docker-compose.prod.yml exec backend bash -c "./manage.py hyfaa_import"
+/usr/local/bin/docker-compose -f docker-compose.yml  -f docker-compose.prod.yml exec -T backend bash -c "./manage.py hyfaa_import"
 
 printf "${ORANGE}\n\n-------------------------------------------------\n"
 printf "Import rain data into DB\n${NORMAL}"
-/usr/local/bin/docker-compose -f docker-compose.yml  -f docker-compose.prod.yml exec backend bash -c "./manage.py rainfall_import"
+/usr/local/bin/docker-compose -f docker-compose.yml  -f docker-compose.prod.yml exec -T backend bash -c "./manage.py rainfall_import"
 
 printf "${ORANGE}\n\n-------------------------------------------------\n"
 printf "last updated: \n${NORMAL}"
